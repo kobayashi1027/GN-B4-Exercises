360a04a81c822bcf4b6c49548a2b34d827a6bb90
Hiroaki Kobayashi <koba1027yashi@gmail.com>
2014-04-09 18:36:29 +0900

    Edit MyTwitterBot.

--- MyTwitterBot.rb
+++ MyTwitterBot.rb
@@ -5,3 +5,12 @@ require './TwitterBot.rb' # TwitterBot.rbの読み込み
 class MyTwitterBot < TwitterBot
   # 機能を追加
 end
+
+tweettest = MyTwitterBot.new
+#tweettest.tweet("TwitterBotのテストです．")
+
+gettest = tweettest.get_tweet
+p gettest[2]["user_id"]
+p gettest[2]["user_name"]
+p gettest[2]["message"]
+p gettest[2]["time"]
